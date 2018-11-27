d9ef1824f36074a2b2548f08324ca2d758d464ba
Graham Sutherland <gsuberland@gmail.com>
2018-11-27 23:45:03 +0000

    Added .NET Core tests

---
+++ Polynomial.Demoscene.DemozooApi.Tests.DotNetCore/Productions.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Polynomial.Demoscene.DemozooApi.Tests.DotNetCore
+{
+    public class Productions
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
