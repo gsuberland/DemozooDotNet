69faaa157d0a54d1cc5f759baa6d7a41f5299322
Graham Sutherland <gsuberland@gmail.com>
2018-11-28 00:06:03 +0000

    Initial test for .NET Core

--- Polynomial.Demoscene.DemozooApi.Tests.DotNetCore/Productions.cs
+++ Polynomial.Demoscene.DemozooApi.Tests.DotNetCore/Productions.cs
@@ -6,9 +6,15 @@ namespace Polynomial.Demoscene.DemozooApi.Tests.DotNetCore
     public class Productions
     {
         [Fact]
-        public void Test1()
+        public void TestFetchProduction()
         {
-
+            var production = DemozooApi.GetProduction(1);
+            Assert.NotNull(production);
+            Assert.Equal(@"https://demozoo.org/api/v1/productions/1/", production.ApiUrl);
+            Assert.Equal(@"https://demozoo.org/productions/1/", production.DemozooUrl);
+            Assert.Equal(1, production.ID);
+            Assert.Equal(@"Rob Is Jarig", production.Title);
+            Assert.Equal(new DateTime(2000, 3, 1), production.ReleaseDate);
         }
     }
 }
